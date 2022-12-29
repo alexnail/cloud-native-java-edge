3489f343ff3a93fcfb7309e65a79a17e23e39a58
alexander.fridman <alexander.fridman@over-c.com>
2022-12-29 13:07:55 +0000

    code reformatted

--- edge-service/src/main/java/greetings/RestTemplateGreetingsClientApiGateway.java
+++ edge-service/src/main/java/greetings/RestTemplateGreetingsClientApiGateway.java
@@ -1,40 +1,42 @@
 package greetings;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
-@Profile({ "default", "insecure" })
+@Profile({"default", "insecure"})
 @RestController
 @RequestMapping("/api")
 class RestTemplateGreetingsClientApiGateway {
 
- private final RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
- @Autowired
- RestTemplateGreetingsClientApiGateway(
-   @LoadBalanced RestTemplate restTemplate) { // <1>
-  this.restTemplate = restTemplate;
- }
+    @Autowired
+    RestTemplateGreetingsClientApiGateway(@LoadBalanced RestTemplate restTemplate) { // <1>
+        this.restTemplate = restTemplate;
+    }
 
- @GetMapping("/resttemplate/{name}")
- Map<String, String> restTemplate(@PathVariable String name) {
+    @GetMapping("/resttemplate/{name}")
+    Map<String, String> restTemplate(@PathVariable String name) {
 
-  //@formatter:off
-  ParameterizedTypeReference<Map<String, String>> type =
-      new ParameterizedTypeReference<Map<String, String>>() {};
-  //@formatter:on
+        //@formatter:off
+        ParameterizedTypeReference<Map<String, String>> type =
+                new ParameterizedTypeReference<Map<String, String>>() {
+                };
+        //@formatter:on
 
-  ResponseEntity<Map<String, String>> responseEntity = this.restTemplate
-   .exchange("http://greetings-service/greet/{name}", HttpMethod.GET, null,
-    type, name);
-  return responseEntity.getBody();
- }
+        ResponseEntity<Map<String, String>> responseEntity
+                = restTemplate.exchange(
+                        "http://greetings-service/greet/{name}", HttpMethod.GET, null, type, name);
+        return responseEntity.getBody();
+    }
 }
